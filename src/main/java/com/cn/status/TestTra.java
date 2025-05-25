8b49a225e16b4607ea20c9ba62cad73e93d89948
cn <1921337965@qq.com>
2025-05-25 08:06:37 +0800

    状态模式

---
+++ src/main/java/com/cn/status/TestTra.java
@@ -0,0 +1,23 @@
+package com.cn.status;
+
+public class TestTra {
+    public static void main(String[] args) {
+        VendingMachine machine = new VendingMachine(10);
+        machine.insertMoney();
+        machine.backMoney();
+
+        System.out.println("-----------");
+
+        machine.insertMoney();
+        machine.turnCrank();
+
+        System.out.println("----------压力测试-----");
+        machine.insertMoney();
+        machine.insertMoney();
+        machine.turnCrank();
+        machine.turnCrank();
+        machine.backMoney();
+        machine.turnCrank();
+
+    }
+}
